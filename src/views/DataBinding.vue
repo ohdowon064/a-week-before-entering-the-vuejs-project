bee559f28aad78c16fec0a160c02a4098e4259cf
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-23 01:34:27 +0900

    data binding list

--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -1,19 +1,17 @@
 <template>
     <div>
-        <select v-model="city">
-            <option value="02">서울</option>
-            <option value="21">부산</option>
-            <option value="064">제주</option>
-        </select>
-        <label><input type="checkbox" v-model="checked" true-value="yes" false-value="no">{{checked}}</label>
+        <label><input type="checkbox" v-model="checked" value="서울"> 서울</label>
+        <label><input type="checkbox" v-model="checked" value="부산"> 부산</label>
+        <label><input type="checkbox" v-model="checked" value="제주"> 제주</label>
+        <br>
+        <span>체크한 지역: {{checked}}</span>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            city: "064",
-            checked: "no"
+            checked: []
         };
     }
 }
