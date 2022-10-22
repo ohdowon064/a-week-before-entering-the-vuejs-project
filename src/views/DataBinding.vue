a52bcf788b13000ba2fc2bbee2865b1d4b2da029
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-23 00:51:19 +0900

    v-html example

---
+++ src/views/DataBinding.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello, {{title}}!</h1>
+    <div>
+        <div>{{htmlString}}</div>
+        <div v-html="htmlString"></div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title: "World",
+            htmlString: '<P style="color:red;">This is a red string.</P>'
+        };
+    }
+}
+</script>
