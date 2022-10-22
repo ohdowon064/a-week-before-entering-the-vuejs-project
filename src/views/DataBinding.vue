98ae0584eab2e997bad4a8a55c76f5f2e3505c6b
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-23 01:04:21 +0900

    data binding: input type=number

--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <input type="text" v-model="valueModel" />
+        <input type="number" v-model.number="numberModel" />
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            valueModel: "South Korea"
+            numberModel: 3
         };
     }
 }
