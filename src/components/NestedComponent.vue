797d54714b56b14ced54ff4d72841995fb876bad
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-24 01:05:57 +0900

    dynamic prop pass

--- src/components/NestedComponent.vue
+++ src/components/NestedComponent.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <PageTitle title="부모 컴포넌트에서 자식 컴포넌트로 데이터 전달"/>
+  <PageTitle :title="title"/>
 </div>
 </template>
 
@@ -8,7 +8,12 @@
 import PageTitle from "@/components/PageTitle";
 export default {
   name: "NestedComponent",
-    components: {PageTitle}
+  components: {PageTitle},
+  data() {
+    return {
+      title: "123"
+    }
+  }
 }
 </script>
 
