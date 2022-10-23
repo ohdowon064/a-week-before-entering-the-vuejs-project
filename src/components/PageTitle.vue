2710eb2e4b69fbeb85519bf7fdc1e839c64cc516
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-24 01:05:44 +0900

    props

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,10 +1,16 @@
 <template>
-<h2>Page Title</h2>
+<h2>{{title}}</h2>
 </template>
 
 <script>
 export default {
-  name: "PageTitle"
+  name: "PageTitle",
+  props: {
+    title: {
+      type: String,
+      default: "페이지 제목입니다."
+    }
+  }
 }
 </script>
 
