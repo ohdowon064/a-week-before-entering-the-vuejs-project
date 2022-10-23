4a3caa55e197009666a7ae3495ab4057be4f5d18
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-23 15:50:25 +0900

    img src bind

--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -1,17 +1,13 @@
 <template>
     <div>
-        <label><input type="checkbox" v-model="checked" value="서울"> 서울</label>
-        <label><input type="checkbox" v-model="checked" value="부산"> 부산</label>
-        <label><input type="checkbox" v-model="checked" value="제주"> 제주</label>
-        <br>
-        <span>체크한 지역: {{checked}}</span>
+        <img v-bind:src="imgSrc"/>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            checked: []
+            imgSrc: "https://v3.ko.vuejs.org/logo.png"
         };
     }
 }
