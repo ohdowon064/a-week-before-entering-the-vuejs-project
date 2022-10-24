b8335624e1507573320adcfd8d72ca6da3c1ab54
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-25 00:54:19 +0900

    computed in composition

--- src/views/CalculatorCompositionAPI.vue
+++ src/views/CalculatorCompositionAPI.vue
@@ -2,9 +2,9 @@
 <div>
 <h2>Calculator</h2>
   <div>
-    <input type="text" v-model="state.num1" @keyup="plusNumbers"/>
+    <input type="text" v-model="state.num1" />
     <span> + </span>
-    <input type="text" v-model="state.num2" @keyup="plusNumbers"/>
+    <input type="text" v-model="state.num2" />
     <span> = </span>
     <span>{{state.result}}</span>
   </div>
@@ -12,7 +12,7 @@
 </template>
 <script>
 
-import {reactive} from "vue";
+import {reactive, computed} from "vue";
 
 export default {
   name: "CalculatorCompositionAPI",
@@ -20,15 +20,9 @@ export default {
     let state = reactive({
       num1: 0,
       num2: 0,
-      result: 0
+      result: computed(() => parseInt(state.num1) + parseInt(state.num2))
     });
-    function plusNumbers() {
-      state.result = parseInt(state.num1) + parseInt(state.num2);
-    }
-    return {
-      state,
-      plusNumbers
-    }
+    return {state}
   }
 }
 </script>
