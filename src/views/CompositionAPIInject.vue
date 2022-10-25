376caf44c39c5ae9dd6533d8755e661da5a3ee1e
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-26 00:53:21 +0900

    provide/inject in setup

---
+++ src/views/CompositionAPIInject.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>{{title}}</h1>
+</template>
+
+<script>
+import {inject} from "vue";
+
+export default {
+  name: "CompositionAPIInject",
+  setup() {
+    const title = inject("title");
+    return {title};
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/CompositionAPIProvide.vue
@@ -0,0 +1,19 @@
+<template>
+<CompositionAPIInject/>
+</template>
+
+<script>
+import CompositionAPIInject from "@/views/CompositionAPIInject";
+import {provide} from "vue";
+export default {
+  name: "CompositionAPIProvide",
+  components: {CompositionAPIInject},
+  setup() {
+    provide("title", "vuejs 프로젝트");
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
