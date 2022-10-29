abc4737bb66364dfba49adb8829723d8933cf9b9
Dowon Oh <dhehdnjs03@gmail.com>
2022-10-29 23:17:44 +0900

    proxy 8080->3000

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: config => {
     config.plugins.delete('prefetch');
+  },
+  devServer: {
+    port: 8080,
+    proxy: {
+        '^/api': {
+          target: 'http://localhost:3000',
+            changeOrigin: true,
+        }
+    }
   }
 })
